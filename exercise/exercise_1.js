cb23535d07a689603ac98d883ab3c80165a644f7
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2017-11-20 10:39:34 +0200

    7 tests passing

--- exercise/exercise_1.js
+++ exercise/exercise_1.js
@@ -1,13 +1,20 @@
 var divide = function(x, y) {
   // todo divide the numbers and handle the case where y is zero (see the tests)
+  if(y===0){
+  	return Error('Cannot divide by zero');
+  }
+  else return x/y;
 }
 
 var divideByThenAdd = function(x, y, z) {
-  var division = divide(x, y)
+  var division = divide(x, y);
 
   // todo handle the case where divide returns an error
+if(division===Error){
+	return Error('Cannot divide by zero');
+}
 
-  return division + z
+  return division + z;
 }
 
 module.exports = {
