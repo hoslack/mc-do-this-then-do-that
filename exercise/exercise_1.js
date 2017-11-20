8c2462af73100c26e5b0934cc1cee26a90799fd8
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2017-11-20 11:13:50 +0200

    All exercise 1 tests passing

--- exercise/exercise_1.js
+++ exercise/exercise_1.js
@@ -1,7 +1,7 @@
 var divide = function(x, y) {
   // todo divide the numbers and handle the case where y is zero (see the tests)
   if(y===0){
-  	return Error('Cannot divide by zero');
+  	return {'isError':true,'message': 'cannot divide by zero' };
   }
   else return x/y;
 }
@@ -10,8 +10,8 @@ var divideByThenAdd = function(x, y, z) {
   var division = divide(x, y);
 
   // todo handle the case where divide returns an error
-if(division===Error){
-	return Error('Cannot divide by zero');
+if(typeof(division)!=='number'){
+	return {'isError':true,'message': 'cannot divide by zero' };
 }
 
   return division + z;
